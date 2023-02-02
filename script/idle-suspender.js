94d81da71d27c22188af44b8f359d2c538af06c5
WoodpeckerZeta <nikolai.kucksdorf@live.de>
2023-02-02 01:14:51 +0100

    A small module to suspend idle sessions

---
+++ script/idle-suspender.js
@@ -0,0 +1,17 @@
+function idleChecker(){
+    let idleTime = eval(Date.now() - localStorage.getItem('last-active-timestamp'));
+    let timeoutLimit = 30;
+    let playerCount = game.users.filter(u => u.active).length;
+    timeoutLimit = eval(timeoutLimit * 60000);
+    if(playerCount = 1 && idleTime > timeoutLimit){ suspend(); }
+};
+
+function lastActivityTimestamp(){ localStorage.setItem('last-active-timestamp', Date.now()); };
+function suspend(){ $('button[data-action="setup"]').click(); };
+
+Hooks.on('init', () => {
+    Hooks.on('renderApplication', lastActivityTimestamp());
+    setInterval(idleChecker(), 60000);
+    lastActivityTimestamp();
+    idleChecker();
+});
